dbaf1d59e7b0c7ecadea4c0ed81943f7bbd0dbee
huteikei <andoromedazadazo11@gmail.com>
2025-03-11 16:05:00 +0900

    画像の配置

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static int day = 0;
+    public static int money = 0;
+    public static int like = 0;
+    public static int cloth = 0;
+    //ClothList
+    public static int lucky = 0;
+    //LuckyQueue
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
